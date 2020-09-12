4b51a084be8add0aa5013eb7ce52fce7c7ea27d1
Paweł <heropn754@gmail.com>
2020-09-12 20:46:26 +0200

    Added top and bottom dynamic colliders

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
 {
 	[SerializeField] public static float gameSpeed = 3.0f;
 
+	[SerializeField] private GameObject topCameraCollider;
+	[SerializeField] private GameObject bottomCameraCollider;
+
 	public static GameManager instance;
 
 	public event Action onGameStarted;
@@ -22,6 +25,18 @@ public class GameManager : MonoBehaviour
 	private void Start()
 	{
 		Player.instance.onCollisionDetected += EndGame;
+
+		//calculating height and witdh of screen and setting up bottom and top colliders
+		Camera camera = Camera.main;
+		float height = 2.0f * camera.orthographicSize;
+		float width = height * camera.aspect;
+
+		topCameraCollider.GetComponent<BoxCollider2D>().size = new Vector2(width, 1);
+		topCameraCollider.transform.position = new Vector2(0, (height / 2) + 0.5f);
+
+		bottomCameraCollider.GetComponent<BoxCollider2D>().size = new Vector2(width, 1);
+		bottomCameraCollider.transform.position = new Vector2(0, -((height / 2) + 0.5f));
+
 	}
 
 	private void Update()
