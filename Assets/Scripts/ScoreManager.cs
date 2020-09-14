ae19ca1cfb1b08cbb0870c64f8a623688039d5e1
Paweł <heropn754@gmail.com>
2020-09-14 19:08:13 +0200

    Added scores

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+	private TextMeshProUGUI textUI;
+	private int scorePoints;
+
+	private void Awake()
+	{
+		textUI = GetComponent<TextMeshProUGUI>();
+	}
+
 	private void Start()
 	{
 		Player.Instance.onTriggerDetected += ScoreAPoint;
+		GameManager.Instance.onGameRestart += RestartPoints;
+		scorePoints = 0;
+		textUI.text = scorePoints.ToString();
 	}
 
 	private void ScoreAPoint()
 	{
+		textUI.text = (++scorePoints).ToString();
+	}
 
+	private void RestartPoints()
+	{
+		scorePoints = 0;
+		textUI.text = scorePoints.ToString();
+	}
+
+	private void OnDestroy()
+	{
+		Player.Instance.onTriggerDetected -= ScoreAPoint;
+		GameManager.Instance.onGameRestart -= RestartPoints;
 	}
 }
