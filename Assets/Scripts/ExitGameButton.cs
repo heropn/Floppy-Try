522d9a3b8667f9e1c71f2c20bc2556a650b4cd98
Paweł <heropn754@gmail.com>
2020-10-08 19:19:50 +0200

    Added Menu Scene

---
+++ Assets/Scripts/ExitGameButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGameButton : MonoBehaviour
+{
+	private Button exitButton;
+
+	private void Awake()
+	{
+		exitButton = GetComponent<Button>();
+	}
+
+	private void Start()
+	{
+		exitButton.onClick.AddListener(Application.Quit);
+	}
+}
