faab9b4fcd533adeb84539fb5bb2db0ca8c5db92
Paweł <heropn754@gmail.com>
2020-10-08 19:32:32 +0200

    Working Menu Scene

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGameButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Button startButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Awake()
+	{
+		startButton = GetComponent<Button>();
+	}
+
+	private void Start()
+	{
+		startButton.onClick.AddListener(LoadGameScene);
+	}
+
+	private void LoadGameScene()
+	{
+		SceneManager.LoadScene("Main", LoadSceneMode.Single);
+	}
 }
